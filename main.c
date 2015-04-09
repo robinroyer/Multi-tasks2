a1ce9578550650116f06d8ad594664461c987b30
Guillaumee <guillaume.sarnette@insa-lyon.fr>
2015-04-09 09:28:53 +0200

    Update main.c
    2 ptreads !! :)

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <inttypes.h>
+#include <pthread.h>
 
 /**
 retourne 1 si p est premier
@@ -23,8 +24,9 @@ int is_prime(uint64_t p)
 	return retour;
 }
 
-void print_prime_factors(uint64_t n)
+void *print_prime_factors(void *p)
 {
+	uint64_t n = (uint64_t)p;
 	printf("%d:",n);
 	uint64_t i;
 	for(i=(uint64_t)2;is_prime(n)==0;i++)
@@ -39,24 +41,72 @@ void print_prime_factors(uint64_t n)
 	printf(" %d\n",n );
 }
 
+/*void *print_prime_factors_bis( void *p)
+{
+	int i;
+	int j;
+	int posI=4;
+	int *m [(int)*p/3];
+	int *dP[(int)*p/3];
+	int sommet=1;
+	int essai;
+	dp[0]=25;
+	m[0]=25;
+
+	for(int i =7;i<*p;i+=posI,posI=6-posI)
+	{
+		for(essai = dp[])
+	}
+
+
+}*/
+
+
+
+
 int main(int argc, char *argv[])
 {
 	uint64_t p;
 	int retour;
+	pthread_t tid1,tid2;
+	int crdu;
 
 	FILE *fichier = NULL;
 
 	fichier = fopen ("number.txt", "r");
 	while (fscanf(fichier, "%d",&p) != EOF) 
 	{
-		print_prime_factors(p);
+		printf("création de thread 1\n");
+		crdu = pthread_create( &tid1, NULL, print_prime_factors, (void *) p);
+		if(crdu!=0)
+		{
+			printf("Erreur de création de thread\n");
+		}
+		//print_prime_factors(p);
+		if(fscanf(fichier, "%d",&p) != EOF)
+		{
+			printf("création de thread 2\n");
+			crdu = pthread_create( &tid2, NULL, print_prime_factors, (void *) p);
+		}
+		if(crdu!=0)
+		{
+			printf("Erreur de création de thread\n");
+		}
+
+		crdu = pthread_join(tid1,NULL);
+		if(crdu!=0)
+		{
+			printf("Erreur de joins de thread 1\n");
+		}
+		crdu = pthread_join(tid2,NULL);
+		if(crdu!=0)
+		{
+			printf("Erreur de joins de thread 2\n");
+		}
+		//print_prime_factors((void *) p);
+
 	}
 	fclose(fichier);
-	/*
-	scanf("%d",&p);
-	retour = is_prime(p);
-	print_prime_factors(p);
-		//printf("%d\n",retour);
-		*/
+	pthread_exit(NULL);
 	return 0;
 }
