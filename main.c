d2113cd83b5c0416ba9f5b405ab9d81079250356
royer <robinroyer@users.noreply.github.com>
2015-04-09 10:33:17 +0200

    update print_prime_factor
    algo pour recherche plus rapide des nombre premier

--- main.c
+++ main.c
@@ -41,24 +41,55 @@ void *print_prime_factors(void *p)
 	printf(" %d\n",n );
 }
 
-/*void *print_prime_factors_bis( void *p)
+
+/* void *print_prime_factors(void *p)
 {
-	int i;
-	int j;
-	int posI=4;
-	int *m [(int)*p/3];
-	int *dP[(int)*p/3];
-	int sommet=1;
-	int essai;
-	dp[0]=25;
-	m[0]=25;
+	uint64_t n = (uint64_t)p;
+	printf(" %d :",n);
+	uint64_t i;
+	int pas=4;
+	
 
-	for(int i =7;i<*p;i+=posI,posI=6-posI)
+	for(i=(uint64_t)7;is_prime(n)==0;i+=(pas=6-pas))
 	{
-		for(essai = dp[])
+
+
+		if(i==7)
+		{
+
+			if(((int)n)%2==0)
+			{
+				printf(" 2 ");
+				n=(uint64_t)((int)n/2);
+				i=(uint64_t)5;
+				pas=4;
+			}
+			if(((int)n)%3==0)
+			{
+				printf("3  ");
+				n=(uint64_t)((int)n/3);
+				i=(uint64_t)5;
+				pas=4;
+			}
+			if(((int)n)%5==0)
+			{
+				printf("5");
+				n=(uint64_t)((int)n/5);
+				i=(uint64_t)5;
+				pas=4;
+			}
+			
+		}
+
+		if(((int)n)%i==0)
+		{
+			printf(" %d",i);
+			n=(uint64_t)((int)n/(int)i);
+			i=(uint64_t)5;
+			pas=4;
+		}
 	}
-
-
+	printf(" %d\n",n );
 }*/
 
 
