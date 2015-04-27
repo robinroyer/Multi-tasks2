2435f78eaae0dc0ac979be5c44283e9244ea440d
royer <robinroyer@users.noreply.github.com>
2015-04-27 14:31:38 +0200

    Update generator.c

--- generator.c
+++ generator.c
@@ -9,18 +9,18 @@ int main(int argc, char *argv[])
     uint64_t *previous_numbers;
 
     // how many numbers to generate
-    int quantity = 20;
+    int quantity = 10;
     if( argc > 1)
         quantity=atoi(argv[1]);
 
     // maximum magnitude of numbers, in bits (0..64)
-    int magnitude= 64;
+    int magnitude= 32;
     if( argc > 2)
         magnitude=atoi(argv[2]);
 
     // percentage of redundancy (0..100)
     // 30% means each number only has 2/3 chance to be a brand new one
-    int redundancy=50;
+    int redundancy=80;
     if( argc > 3)
         redundancy=atoi(argv[3]);
 
